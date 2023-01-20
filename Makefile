da93bc2e89bf66beb56ba79603ee201b95c1efe3
chua sung hui <neil_sundae@Neil-bb.localdomain>
2023-01-20 23:18:40 +0800

    added Makefile

---
+++ Makefile
@@ -0,0 +1,33 @@
+NAME		=	philosopher
+MAN_FILES	=	main
+MAN_DIR		=	philo/
+MOBJS_DIR	=	man_objs/
+MAN_SRCS	=	$(addprefix $(MAN_DIR), $(addsuffix .c, $(MAN_FILES)))
+MAN_OBJS	=	$(addprefix $(MOBJS_DIR), $(addsuffix .o, $(MAN_FILES)))
+GCC			=	gcc
+CFLAGS		=	-Wall -Werror -Wextra
+FSAN		=	-fsanitize=address -g3
+RED			:=	$(shell tput -Txterm setaf 1)
+RM			=	-rm -rf
+
+all:
+	@mkdir -p $(MOBJS_DIR)
+	@make $(NAME)
+	@echo "$(RED)philo ready HEHHE..."
+
+$(MOBJS_DIR)%.o : $(MAN_DIR)%.c
+	@$(GCC) -c $< -o $@
+
+$(NAME): $(MAN_OBJS)
+	@$(GCC) $(CFLAGS) $(MAN_OBJS) $(FSAN) -o $(NAME)
+
+clean:
+	@$(RM) $(MOBJS_DIR)
+
+fclean:
+	@make clean
+	@$(RM) $(NAME)
+
+re:
+	@make fclean
+	@make all
---
+++ philo/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: suchua < suchua@student.42kl.edu.my>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/20 22:10:29 by suchua            #+#    #+#             */
+/*   Updated: 2023/01/20 22:10:29 by suchua           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
